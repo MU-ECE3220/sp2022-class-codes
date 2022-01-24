40bcf23140cff2e2e5e1c6970a9a49ae5f9cb349
Ekincan Ufuktepe <ekincanufuktepe@gmail.com>
2022-01-24 13:29:47 -0600

    Added compiling steps outputs
    These files are compiling outputs from the "main.c" file.
    Includes pre-processing, assembly, object file.

---
+++ week_02/main.s
@@ -0,0 +1,16 @@
+	.file	"main.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
